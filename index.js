626a60e6c07a6c7865fe210af9305f7e96cc6506
kennansalisbury <kennansalisbury@gmail.com>
2019-12-18 13:44:04 -0800

    code-along

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer')
+var cloudinary = require('cloudinary')
+
 var app = express();
+var upload = multer({dest: './uploads/'})
+
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -8,5 +14,15 @@ app.use(ejsLayouts);
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+// app.post('/', upload.single('myFile'), (req, res) => {
+//   res.send(req.file)
+// })
+
+app.post('/', upload.single('myFile'), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    res.send(result)
+  })
+})
 
 app.listen(3000);
